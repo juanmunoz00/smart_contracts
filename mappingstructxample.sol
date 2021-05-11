7bd746d432ee4df2fa6bd074d09a2987f5a2cf1a
Juan Munoz <jmunoz@syner.info>
2021-05-10 23:23:18 -0600

    Using mappings with structures for balance transfer.

--- mappingstructxample.sol
+++ mappingstructxample.sol
@@ -3,7 +3,21 @@ pragma solidity ^0.5.13;
 contract MappingStructureExample{
     //A mapping works as a dictionary with a key and a value;
     //This mapping is to pair an address and its account balance
-    mapping(address => uint) public balanceReceived;
+    
+    //In this iteration 2 structures will be used to handle transactions more efficiently
+    struct Payment{
+        uint amount;
+        uint timeStamps;
+    }
+    
+    struct Balance{
+        uint totalBalance;
+        uint numPayments; //Struct index
+        mapping(uint => Payment) payments;
+    }
+    
+    //mapping(address => uint) public balanceReceived;
+    mapping(address => Balance) public balanceReceived;
     
     function getBalance() public view returns(uint){
         return address(this).balance;
@@ -11,21 +25,30 @@ contract MappingStructureExample{
     
     function sendMoney() public payable{
         //A payable specifies will be dealing with addresses
-        balanceReceived[msg.sender] += msg.value;
+        //balanceReceived[msg.sender] += msg.value;
+        
+        balanceReceived[msg.sender].totalBalance += msg.value;
+        
+        //Referenced typed (??) this is stored in memory
+        Payment memory payment = Payment(msg.value, now);
+        //The payment value is stored in the Balance structure with the numPayments index
+        balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments] = payment;
+        balanceReceived[msg.sender].numPayments++;
+        
     }
     
     function withdrawMoney(address payable _to, uint amount) public{
         //Checks effects interaction patter
-        require(balanceReceived[msg.sender] >= amount, "not enough funds");
-        balanceReceived[msg.sender] -= amount;
+        require(balanceReceived[msg.sender].totalBalance >= amount, "not enough funds");
+        balanceReceived[msg.sender].totalBalance -= amount;
         _to.transfer(amount);
     }
     
     function withdrawAllMoney(address payable _to) public{
         //Checks effects interaction patter
         //Interaction with the SC comes last, everything needs to be updated first 
-        uint balanceToSend = balanceReceived[msg.sender];
-        balanceReceived[msg.sender] = 0;
+        uint balanceToSend = balanceReceived[msg.sender].totalBalance;
+        balanceReceived[msg.sender].totalBalance = 0;
         _to.transfer(balanceToSend);
     }
     
