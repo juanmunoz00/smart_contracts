ffa5499638a5d434750f8002669fdefcbd559ae6
Juan Munoz <jmunoz@syner.info>
2021-05-16 16:22:59 -0600

    Using a for in a library.

---
+++ libraries2.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.4.16 <0.8.0;
+
+library Search {
+    function indexOf(uint[] storage self, uint value)
+        public
+        view
+        returns (uint)
+        {
+            for(uint i = 0; i < self.length; i++)
+                if (self[i] == value) return i;
+            return uint(-1);
+        }
+}
+
+contract NotUsingForExample{
+    uint[] data;
+    
+    function append(uint value) public{
+        data.push(value);
+    }
+    
+    function replace(uint _old, uint _new) public{
+        //This perforfms the library function call
+        uint index = Search.indexOf(data, _old);
+        if(index == uint(-1))
+            data.push(_new);
+        else
+            data[index] = _new;
+    }
+}
+
+contract UsingForExample {
+    using Search for uint[];
+    uint[] data;
+    
+    function append(uint value) public{
+        data.push(value);
+    }
+}
