82c1d395d2ea807ad56c4ea47a7070e030a3fb6b
Juan Munoz <jmunoz@syner.info>
2021-05-16 14:45:28 -0600

    Events example.

---
+++ events.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.13;
+
+contract EventExample {
+    mapping(address => uint) public tokenBalance;
+    
+    //An event is used to send a message to the contract starter
+    event TokensSent(address _from, address _to, uint _amount);
+    
+    constructor() public{
+        tokenBalance[msg.sender] = 100;
+    }
+    
+    function SendToken(address _to, uint _amount) public returns(bool){
+        require(tokenBalance[msg.sender] >= _amount, "not enough tokens");
+        assert(tokenBalance[_to] + _amount >= tokenBalance[_to]);
+        assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
+        tokenBalance[msg.sender] -= _amount;
+        tokenBalance[_to] += _amount;
+        
+        emit TokensSent(msg.sender, _to, _amount);
+        
+        return true;
+    }
+}
