843239941e4a2c42cc8f1486058c60b7d686ea57
Juan Munoz <jmunoz@syner.info>
2021-05-16 22:29:35 -0600

    Complete shared wallet SC.

--- shared_wallet/simplewallet.sol
+++ shared_wallet/simplewallet.sol
@@ -1,13 +1,29 @@
-pragma solidity ^0.5.13;
+//SPDX-License-Identifier: MIT
 
-contract SimpleWallet{
-    
-    function withdrawMoney(address payable _to, uint _amount) public {
+pragma solidity 0.8.1;
+
+import "Allowance.sol";
+
+contract SharedWallet is Allowance {
+
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "Contract doesn't own enough money");
+        if(!isOwner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
+
     }
-    
-    function () external payable {
-        
+
+    function renounceOwnership() public override onlyOwner {
+        revert("can't renounceOwnership here"); //not possible with this smart contract
+    }
+
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
-
