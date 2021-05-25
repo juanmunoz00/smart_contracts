d3ffe18bdf274d7fd9445eccd795e2e4e6450202
Juan Munoz <jmunoz@syner.info>
2021-05-24 18:06:33 -0600

    Payment handling done.

--- Supply_Chain/eventtrigger.sol
+++ Supply_Chain/eventtrigger.sol
@@ -1,5 +1,44 @@
 pragma solidity ^0.6.0;
 
+contract Item{
+    uint public priceInWei;
+    uint public pricePaid;
+    uint public index;
+    
+    ItemManager partentContract;
+    
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public{
+        priceInWei = _priceInWei;
+        index = _index;
+        partentContract = _parentContract;
+    }
+    
+    /* Callback function to handle the money receipet to send it to Item Manager */
+    receive() external payable{
+        //address(partentContract).transfer(msg.value);
+        /* We'll use a called 'low level function' to ensure transaction has the 
+            needed amount of gas to be executed
+            
+            Call the trigger payment to handle the payment.
+            
+            It's CRUCIALLY IMPORTANT to get the return value...
+            
+            The call gives you two return values:
+            1) A boolean if the transaccion was successful an 
+            2) The return of the function
+            
+        */
+        require(pricePaid == 0, "Item's already paid !");
+        require(priceInWei == msg.value, "Only full payments allowed");
+        pricePaid += msg.value;
+        (bool success, ) = address(partentContract).call.value(msg.value)(abi.encodeWithSignature("triggerPayment(uint256)", index));
+        require(success, "The transaction was not successful, canceling!");
+    }
+    
+    fallback() external {} //Interact from external with contract.
+    
+}
+
 contract ItemManager{
     
     /* Enumeration to define an item state/stage/phase */
@@ -8,12 +47,13 @@ contract ItemManager{
     enum SupplyChainState{ Created, Paid, Delivered }
     
     struct S_Item {
+        Item _item;
         string _identifier;
         uint _itemPrice;
         ItemManager.SupplyChainState _state;
     }
     
-    event SupplyChainStep(uint _itemIndex, uint _step);
+    event SupplyChainStep(uint _itemIndex, uint _step, address _itemAddress);
     
     /* Structure handling */
     mapping(uint => S_Item) public items;
@@ -21,10 +61,13 @@ contract ItemManager{
     
     /* Create an item with the provided attributes */
     function createItem(string memory _identifier, uint _itemPrice) public{
+        Item item = new Item(this, _itemPrice, itemIndex); //Instantiating the contract that'll handle the items
+        
+        items[itemIndex]._item = item;
         items[itemIndex]._identifier = _identifier;
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
-        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state));//Communicate to the "outside world" (SC)
+        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state), address(item));//Communicate to the "outside world" (SC)
         itemIndex++;
         
     }
@@ -38,7 +81,7 @@ contract ItemManager{
         require(items[_itemIndex]._state == SupplyChainState.Created, "Item is further in the chain !");
         items[_itemIndex]._state = SupplyChainState.Paid;
         
-        emit SupplyChainStep(itemIndex, uint(items[_itemIndex]._state));//Communicate to the "outside world" (SC)
+        emit SupplyChainStep(itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));//Communicate to the "outside world" (SC)
     }
     
     function triggerDelivery(uint _itemIndex) public {
@@ -46,7 +89,7 @@ contract ItemManager{
         require(items[_itemIndex]._state == SupplyChainState.Created, "Item is further in the chain !");
         items[_itemIndex]._state = SupplyChainState.Paid;
         
-        emit SupplyChainStep(itemIndex, uint(items[_itemIndex]._state));//Communicate to the "outside world" (SC)
+        emit SupplyChainStep(itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));//Communicate to the "outside world" (SC)
     }
     
 }
