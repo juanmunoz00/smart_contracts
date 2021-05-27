e75a9610a52f3beac6e2dbec4333407f913290d2
Juan Munoz <jmunoz@syner.info>
2021-05-27 14:18:09 -0600

    Initial version working correctly.

--- truffle_example1/client/src/App.js
+++ truffle_example1/client/src/App.js
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+//import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import ItemmanagerContract from './contracts/ItemManager.json';
+import ItemContract from './contracts/Item.json';
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { loaded: false, cost: 0, itemName:"example_1"};
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+      this.networkId = await this.web3.eth.net.getId();
+      //const deployedNetwork = ItemmanagerContract.networks[networkId];
+      this.itemmanager = new this.web3.eth.Contract(
+        ItemmanagerContract.abi,
+        ItemmanagerContract.networks[this.networkId] && ItemmanagerContract.networks[this.networkId].address,
+        //deployedNetwork && deployedNetwork.address,
+      );
+
+      this.item = new this.web3.eth.Contract(
+        ItemContract.abi,
+        ItemContract.networks[this.networkId] && ItemContract.networks[this.networkId].address,
+        //deployedNetwork && deployedNetwork.address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ loaded:true });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,36 +44,42 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+handleInputChange = (event) => {
+  
+  const target = event.target;
+  //Establish the item value
+  const value = target.type === "checkbox" ? target.checked : target.value;
+  const name = target.name;
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+  //Establish the state
+  this.setState({
+    [name]: value
+  });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+}
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+handleSubmit = async() => {
+
+  const { cost, itemName } = this.state;
+  //Send this values to the SC in the BCH.
+  await this.itemmanager.methods.createItem(itemName, cost).send({from: this.accounts[0]});
+
+}
 
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+
+        <h1>Event Trigger / Supply Chain Example</h1>
+        <h2>Items</h2>
+        <h2>Add Items</h2>
+        Cost in Wei: <input type="text" name="cost" value={ this.state.cost } onChange={this.handleInputChange} />
+        Item Identifier: <input type="text" name="itemName" value={ this.state.item } onChange={this.handleInputChange} />
+        <button type="button" onClick={this.handleSubmit}>Create a new item</button>
+
       </div>
     );
   }
--- truffle_example1/migrations/2_deploy_contracts.js
+++ truffle_example1/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Itemmanager = artifacts.require("./Itemmanager.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Itemmanager);
 };
